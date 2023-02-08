d1f754ea2ccaf1a7f0b11a6890346e7852d4781b
unknown <tomygarnero9429@gmail.com>
2023-02-08 14:20:38 -0300

    Show more info from API

--- js/app.js
+++ js/app.js
@@ -69,17 +69,35 @@ function consultarApi(ciudad, pais) {
 }
 
 function mostrarClima(datos) {
-    const { main: {temp, temp_max, temp_min}}= datos;
+    const { name, main: {temp, temp_max, temp_min}}= datos;
 
     const centigrados = kelvinACentigrados(temp);
+    const max = kelvinACentigrados(temp_max);
+    const min = kelvinACentigrados(temp_min);
+
+    const nombreCiudad = document.createElement("p");
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add("font-bold", "text-2xl");
     
     const actual = document.createElement("p");
     actual.innerHTML = `${centigrados} &#8451;`;
     actual.classList.add("font-bold", "text-6xl");
 
+    const tempMax = document.createElement("p");
+    tempMax.innerHTML = `Max: ${max} &#8451;`;
+    tempMax.classList.add("text-xl");
+
+    const tempMin = document.createElement("p");
+    tempMin.innerHTML = `Min: ${max} &#8451;`;
+    tempMin.classList.add("text-xl");
+
+
     const resultadoDiv = document.createElement("div");
     resultadoDiv.classList.add("text-center", "text-white");
+    resultadoDiv.appendChild(nombreCiudad);
     resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMax);
+    resultadoDiv.appendChild(tempMin);
 
     resultado.appendChild(resultadoDiv);
 }
